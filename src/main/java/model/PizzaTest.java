431acc5e04e1dc5449c4f70be24208e1538b73ab
Serejenka <mikheevs11@gmail.com>
2019-06-24 19:43:25 +0300

    Added 2 tests

---
+++ src/main/java/model/PizzaTest.java
@@ -0,0 +1,38 @@
+package model;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PizzaTest {
+
+    private static Pizza pizza;
+
+    @BeforeClass
+    public static void setUp() {
+        pizza = new Pizza();
+    }
+
+    @Test
+    public void testAddIngredientOnCheese() {
+        Ingredient cheese = new Ingredient("Cheese");
+        try {
+            pizza.addIngredient(cheese, 1);
+        } catch (PizzaException e) {
+            e.printStackTrace();
+        }
+        assertEquals(1, pizza.getIngredients().get(cheese).intValue());
+    }
+
+    @Test
+    public void testRemoveIngredientOnCheese() {
+        Ingredient cheese = new Ingredient("Cheese");
+        try {
+            pizza.removeIngredient(cheese, 1);
+        } catch (PizzaException e) {
+            e.printStackTrace();
+        }
+        assertEquals(null, pizza.getIngredients().get(cheese));
+    }
+}
